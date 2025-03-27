e0dc82d31829870c21fe4c5b2f00d133f7d663ee
sj <muhommodshahjalal@gmail.com>
2025-03-27 22:40:21 +0600

    up

--- src/app/module/student/student.service.ts
+++ src/app/module/student/student.service.ts
@@ -1,4 +1,9 @@
+import mongoose from "mongoose";
+import httpStatus from "http-status";
 import { Student } from "./student.model";
+import AppError from "../../Errors/AppError";
+import { User } from "../user/user.model";
+import { TStudent } from "./student.interface";
 
 /* export const createStudentToDb = async (studentData: Student) => {
   // const result = await StudentModel.create(studentData); // built in static method
@@ -44,17 +49,90 @@ const getStudentByIdFromDb = async (id: string) => {
 };
 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne(
-    { id },
-    {
-      isDeleted: true,
-    },
-  );
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Failed to delete student");
+    }
+
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Failed to delete user");
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deletedStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error("Failed to delete student");
+  }
+};
+
+const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
+  const { name, guardian, localGuardian, ...remainingStudentData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+
+  /*
+    guardain: {
+      fatherOccupation:"Teacher"
+    }
+
+    guardian.fatherOccupation = Teacher
+
+    name.firstName = 'Mezba'
+    name.lastName = 'Abedin'
+  */
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
+      modifiedUpdatedData[`guardian.${key}`] = value;
+    }
+  }
+
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
+    }
+  }
+
+  console.log(modifiedUpdatedData);
+
+  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
   return result;
 };
 
 export const StudentService = {
   getStudentByIdFromDb,
-  deleteStudentFromDB,
   getAllStudentsFromDb,
+  deleteStudentFromDB,
+  updateStudentIntoDB,
 };
